f46fcc0ea262a27f236e3678e7b086a965a8d68b
manvithlb <manvithlb@gmail.com>
2024-12-01 05:27:34 +0530

    Added JWT Authorisation

---
+++ src/main/java/com/agritrading/AgritradingApplication/model/Users.java
@@ -0,0 +1,47 @@
+package com.agritrading.AgritradingApplication.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Users {
+    @Id
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    private String password;
+}
