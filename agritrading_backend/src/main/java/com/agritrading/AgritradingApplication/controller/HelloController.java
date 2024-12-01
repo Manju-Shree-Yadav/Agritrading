64e6d6651175b08119dcfd88ded62daf3c8d7a9b
manvithlb <manvithlb@gmail.com>
2024-12-02 00:05:15 +0530

    Added two new roles farmers and customers

--- agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/controller/HelloController.java
+++ agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/controller/HelloController.java
@@ -1,14 +1,26 @@
 package com.agritrading.AgritradingApplication.controller;
 
+import com.agritrading.AgritradingApplication.model.Users;
+import com.agritrading.AgritradingApplication.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController  {
+    @Autowired
+    private UserRepo userRepo;
 
     @GetMapping("/")
-    public String greet() {
-        return "Hello World";
+    public String greet(Authentication authentication) {
+        String username = authentication.getName();
+
+        // Fetch user details from the database
+        Users user = userRepo.findByUsername(username);
+
+        // Return a personalized greeting
+        return "Hello, " + user.getUsername() + "!";
     }
 
 
--- agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/controller/UserController.java
+++ agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/controller/UserController.java
@@ -2,6 +2,7 @@ package com.agritrading.AgritradingApplication.controller;
 
 
 import com.agritrading.AgritradingApplication.model.Users;
+import com.agritrading.AgritradingApplication.service.RegisterRequest;
 import com.agritrading.AgritradingApplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +16,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/register")
-    public  Users register(@RequestBody Users user) {
+    public  Users register(@RequestBody RegisterRequest user) {
         return userService.register(user);
     }
 
