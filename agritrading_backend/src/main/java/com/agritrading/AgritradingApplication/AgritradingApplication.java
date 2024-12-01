7cf244d8efda436e89d06d6c3145d52e0022f1e4
manvithlb <manvithlb@gmail.com>
2024-12-01 05:40:03 +0530

    reorganised filed

---
+++ agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/AgritradingApplication.java
@@ -0,0 +1,12 @@
+package com.agritrading.AgritradingApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgritradingApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AgritradingApplication.class, args);
+	}
+}
+
---
+++ agritrading_backend/src/main/java/com/agritrading/AgritradingApplication/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.agritrading.AgritradingApplication.controller;
+
+
+import com.agritrading.AgritradingApplication.model.Users;
+import com.agritrading.AgritradingApplication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/register")
+    public  Users register(@RequestBody Users user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public  String login(@RequestBody Users user) {
+        return userService.verify(user);
+    }
+}
